97283435e197bd8ee966c189deeef6c1b64b9d00
Gus <gdiazbanuelos@gmail.com>
2023-03-24 11:30:44 -0500

    removed some trash files

--- demo.py
+++
@@ -1,8 +0,0 @@
-import re
-
-string = "This is a sample string with multiple (ID:12345) patterns (ID:67890)"
-pattern = r"\(ID:\d+\)"
-
-result = re.sub(pattern, lambda match: str(max(map(int, re.findall(r'\d+', match.group())))), string)
-
-print(result)
